0e44ddc4a2814621c85c0455510f23812559c7e6
澤田大樹 <hiroki.bigtree@icloud.com>
2021-02-25 22:52:39 +0900

    トップページに記事一覧を表示する準備2

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -33,6 +33,23 @@
     </div>
 </div>
 @else
+
+<div class="top-wrapper">
+    <div class="articles-wrapper col-md-6">
+      @foreach ($articles as $article)
+      <div class="article-box">
+        <div class="article-box-left"></div>
+        <div class="article-box-right">
+          <a class="article-title" href="/drafts/{{ $article->id }}">{{ $article->title }}</a>
+          <div class="article-details">
+            <div class="article-date">{{ $article->created_at }}</div>
+          </div>
+        </div>
+      </div>
+      @endforeach
+    </div>
+</div>
+
 @endif
 
 @endsection
