08f5710cef478262291c151112bf0cbf7b04d9ac
澤田大樹 <hiroki.bigtree@icloud.com>
2021-02-25 21:39:18 +0900

    個別の記事を表示する画面のレイアウトを作成

--- resources/views/auth/item.blade.php
+++ resources/views/auth/item.blade.php
@@ -1,4 +1,24 @@
 @extends('layouts.common')
 @section('content')
 
+<div class="item-page-wrapper">
+    <div class="item-wrapper">
+        <div class="item-header">
+            <div class="date">{{$article->created_at}}</div>
+        </div>
+        <div class="item-title">{{$article->title}}</div>
+        <div class="item-tags">
+            <div class="item-tag">{{$article->tag1}}</div>
+            @if ($article->tag2)
+            <div class="item-tag">{{$article->tag2}}</div>
+            @endif
+
+            @if ($article->tag3)
+            <div class="item-tag">{{$article->tag3}}</div>
+            @endif
+        </div>
+        <div class="item-body">{{$article->body}}</div>
+    </div>
+</div>
+
 @endsection
