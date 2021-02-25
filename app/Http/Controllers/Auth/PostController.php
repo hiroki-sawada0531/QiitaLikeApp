b49cd3b00f19c46f57e6a6940cf982d9023888ac
澤田大樹 <hiroki.bigtree@icloud.com>
2021-02-25 12:34:21 +0900

    エラーの表示を設定

--- app/Http/Controllers/Auth/PostController.php
+++ app/Http/Controllers/Auth/PostController.php
@@ -4,11 +4,38 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Post;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
     public function index() {
         return view ('auth.drafts.new');
     }
-    
+
+    public function postArticle(Request $request)
+    {
+
+        $request->validate([
+            'title'=> 'required|unique:posts|max:255',
+            'tags' => 'required|string',
+            'article' => 'required|string',
+        ]);
+
+        $tags = explode('', $request->tags);
+        $tag1 = $tags[0];
+        $tag2 = (isset($tags[1]))?$tags[1]:null;
+        $tag3 = (isset($tags[2]))?$tags[2]:null;
+
+        $article = Post::create([
+            'user_id' => Auth::user()->id,
+            'title' => $request->title,
+            'tag1' => $tag1,
+            'tag2' => $tag2,
+            'tag3' => $tag3,
+            'body' => $request->article,
+        ]);
+        return redirect('/');
+    }
+
 }
--- resources/views/auth/drafts/new.blade.php
+++ resources/views/auth/drafts/new.blade.php
@@ -2,8 +2,23 @@
 @section('content')
 <form class="post-page-wrapper" action="/drafts/new" method="post">
 @csrf
+
+    @if($errors->first('title'))
+        <div class="validation">{{ $errors->first('title') }}</div>
+    @endif
+
     <input type="text" class="form-control m-1" id="title-input" placeholder="タイトル" name="title">
+
+    @if($errors->first('tags'))
+        <div class="validation">{{ $errors->first('tags') }}</div>
+    @endif
+
     <input type="text" class="form-control m-1" placeholder="プログラミング技術に関するタグをスペース区切りで3つまで入力" name="tags">
+
+    @if($errors->first('body'))
+        <div class="validation">{{ $errors->first('article') }}</div>
+    @endif
+    
     <div class="row">
         <div class="col-6 m-1">
             <textarea name="article" id="markdown_editor_textarea" cols="30" rows="10" class="form-control"></textarea>
