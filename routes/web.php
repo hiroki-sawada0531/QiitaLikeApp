5719c80097203569f39df3b87ec52b27ce1eaa95
澤田大樹 <hiroki.bigtree@icloud.com>
2021-02-25 20:14:56 +0900

    個別の記事画面のルーティングを作成

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/drafts/new', 'Auth\PostController@index')->name('drafts.new');
 
 Route::post('/drafts/new', 'Auth\PostController@postArticle')->name('drafts.new.posts'); 
+
+Route::get('/drafts/{id}', 'Auth\PostController@showArticle')->name('item');
